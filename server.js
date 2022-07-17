762866ab27923820e5284b7d7a927a5da916ae8d
Dongido <chima.sima@gmail.com>
2022-07-17 15:51:06 +0100

    update server js file

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 //Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(express.static('./dist/'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/CreditWallet'));
 
-app.get('/*', (req, res) => {
-  res.sendFile('index.html', { root: 'dist/' });
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/CreditWallet/'}),
+);
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
