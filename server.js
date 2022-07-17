6ec84a0447123d9e5ec12ba78888a8a182ce4ebc
Dongido <chima.sima@gmail.com>
2022-07-17 12:39:27 +0100

    modify server file

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/credit-wallet/dist'));
+app.use(express.static('./dist/'));
 
-app.get('*', (req, res) => {
-  res.sendFile(`./credit-wallet/dist/index.html`); 
+
+app.get('/*', (req, res) => {
+  res.sendFile('index.html', { root: 'dist/' });
 });
 
 // Start the app by listening on the default Heroku port
